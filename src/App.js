828ddee68e6a742198125efa4ed236b1b8c1a7c4
taekwonkim <taekwundo@gmail.com>
2022-11-23 18:26:00 +0900

    til

--- src/App.js
+++ src/App.js
@@ -14,51 +14,6 @@ const Wrapper = ({children}) => {
 const Block = (val) => <div className={val}></div>;
 // const Block =  () => <div className='h10 m-tb10'></div>;
 
-const Header = (props) => {
-  console.log('props',props, props.title);
-  return(
-    <header>
-      <h1>
-        <a href="/" 
-        onClick={(event) => {
-          event.preventDefault();
-          props.onChangeMode();
-        }}>
-        {props.title}</a>
-      </h1>
-    </header>
-  );
-}
-
-const List = (props) => {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} 
-        onClick={(event) => {
-          event.preventDefault();
-          props.onChangeMode(event.target.id);
-        }}
-      >
-      {t.title}</a></li>);
-  }
-  return(
-    <ol>
-      {lis}
-    </ol>
-  );
-}
-
-const Article = (props) => {
-  return(
-    <article>
-      <p>{props.title}</p>
-      <p>{props.body}</p>
-    </article>
-  );
-}
-
 const Plus = () => {
   const[number,setNumber] = useState(0);
   const add = () => setNumber(number+1);
@@ -95,22 +50,96 @@ const Typekey = () => {
   );
 }
 
+const Header = (props) => {
+  return(
+    <header>
+      <h1>
+        <a href="/" 
+        onClick={(event) => {
+          event.preventDefault();
+          props.onChangeMode();
+        }}>
+        {props.title}</a>
+      </h1>
+    </header>
+  );
+}
+
+const List = (props) => {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} 
+        onClick={(event) => {
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));
+        }}
+      >
+      {t.title}</a></li>);
+  }
+  return(
+    <ol>
+      {lis}
+    </ol>
+  );
+}
+
+const Article = (props) => {
+  return(
+    <article>
+      <p>{props.title}</p>
+      <p>{props.body}</p>
+    </article>
+  );
+}
+
 function App() {
   const topics = [
     {id:1, title:'list1', body:'list array 1'},
     {id:2, title:'list2', body:'list array 2'},
     {id:3, title:'list3', body:'list array 3'},
   ];
+
+  // const _mode = useState('welcome');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState('welcome');
+  const [id, setId] = useState(null);
+  let content = null;
+  if (mode === 'welcome') {
+    content = <Article title='welcome' body='hello, welcome'></Article>;
+  } else if(mode === 'read') {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+      
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
+
   return (
     <Wrapper>
         <Header title='react' 
         onChangeMode={() => {
-           alert('Hi');
+          //  alert('Hi');
+          setMode('welcome');
         }}>
         </Header>
         <Article title='Article title' body='Article body'></Article>
         <Article title='Article2 title' body='Article2 body'></Article>
-        <List topics={topics} onChangeMode={(id) => {alert(id)}}></List>
+        <List topics={topics} 
+        onChangeMode={(_id) => {
+          // alert(id);
+          setMode('read');
+          setId(_id);
+        }}>
+        </List>
+        {content}
+        {Block('h10 m-tb10')}
         <Plus></Plus>
         {Block('h10 m-tb10')}
         {Typekey()}
