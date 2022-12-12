8db2e9b44c70844089a88edba6915a9ec8c4f3aa
taekwonkim <taekwundo@gmail.com>
2022-12-12 18:31:56 +0900

    til

--- src/pages/soaple/Soaple.js
+++ src/pages/soaple/Soaple.js
@@ -322,11 +322,12 @@ const TemperatureInput = (props) => {
 }
 
 const BoilingVerdict = (props) => {
-  if (props.cesius >= 100) {
+  if (props.celsius >= 100) {
     return <p>물이 끓습니다.</p>;
   }
   return <p>물이 끓지 않습니다.</p>;
 };
+
 const toCelsius = (fahrenheit) => {
   return ((fahrenheit-32)*5)/9;
 };
@@ -338,12 +339,34 @@ const tryConvert = (temperature, convert) => {
   if (Number.isNaN(input)) {
     return '';
   }
+  const output = convert(input);
+  const rouded = Math.round(output*1000)/1000;
+  return rouded.toString();
 };
+
 const Calculator = (props) => {
+  const [temperature, setTemperature] = useState('');
+  const [scale, setScale] = useState('c');
+
+  const handleCelsiusChange = (temperature) => {
+    setTemperature(temperature);
+    setScale('c');
+  };
+  const handleFahrenheitChange = (temperature) => {
+    setTemperature(temperature);
+    setScale('f');
+  };
+
+  const celsius = 
+    scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+  const fahrenheit = 
+    scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
   return(
     <div>
-      <TemperatureInput scale='c'></TemperatureInput>
-      <TemperatureInput></TemperatureInput>
+      <TemperatureInput scale='c' temperature={celsius} onTemperatureChange={handleCelsiusChange}></TemperatureInput>
+      <TemperatureInput scale='f' temperature={fahrenheit} onTemperatureChange={handleFahrenheitChange}></TemperatureInput>
+      <BoilingVerdict celsius={parseFloat(celsius)}></BoilingVerdict>
     </div>
   );
 }
